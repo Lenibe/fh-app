a7f7d24a20e692d3b95b43e13bf9b5ef3658ddcb
BabsiZartl <32221064+BabsiZartl@users.noreply.github.com>
2018-01-04 14:57:58 +0100

    Accordion news.html

--- news/js/news.js
+++ news/js/news.js
@@ -9,3 +9,20 @@
         js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.11';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
+
+
+
+var acc = document.getElementsByClassName("btn-sm");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var p = this.nextElementSibling;
+    if (p.style.maxHeight){
+      p.style.maxHeight = null;
+    } else {
+      p.style.maxHeight = p.scrollHeight + "px";
+    } 
+  });
+}
