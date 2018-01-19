21ce1bfdd4696273496236e6cf81468088b531a6
Igor Ivkic <igor.ivkic@fh-burgenland.at>
2018-01-19 17:52:30 +0100

    Igor commit

--- pers/js/pers.js
+++ pers/js/pers.js
@@ -10,7 +10,7 @@ function searchPerson() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("personList");
     li = ul.getElementsByTagName("li");
-        
+
     for (i = 0; i < li.length; i += 1) {
         strong = li[i].getElementsByTagName("strong").item(0);
 
@@ -24,9 +24,9 @@ function searchPerson() {
 
 function setStyles() {
     'use strict';
-    
+
     var pList = document.getElementById('pList');
-    
+
     pList.style.listStyleType = "none";
     pList.style.textAlign = "center";
 }
@@ -38,13 +38,13 @@ function processPersonData(data) {
         singleRow = 0,
         rowCells,
         persList,
-        pListIconsNode, 
-        pListRowsNode, 
-        pDivImageNode, 
-        pImageNode, 
-        pDivContentNode, 
-        pDivPersonNameNode, 
-        pStrongNode, 
+        pListIconsNode,
+        pListRowsNode,
+        pDivImageNode,
+        pImageNode,
+        pDivContentNode,
+        pDivPersonNameNode,
+        pStrongNode,
         pStrongTextNode,
         pDivPersonFunctionNode,
         pSpanPersonFunctionNode,
@@ -65,17 +65,20 @@ function processPersonData(data) {
         pAPersonPhoneTextNode,
         strMail,
         resMail,
-        pHR;
+        pHR,
+        pDivNaviNode,
+        pNaviNode,
+        pNaviTextNode;
 
     persList = document.getElementById('personList');
-    
+
     for (singleRow = 0; singleRow < allRows.length; singleRow += 1) {
         rowCells = allRows[singleRow].split(';');
-        
+
         pListIconsNode = document.createElement('li');
         pListIconsNode.setAttribute('id', 'pListIcons');
         persList.appendChild(pListIconsNode);
-        
+
         pListRowsNode = document.createElement('div');
         pListRowsNode.setAttribute('id', 'pListRows');
         pListRowsNode.setAttribute('class', 'pListRows');
@@ -85,18 +88,18 @@ function processPersonData(data) {
         pDivImageNode = document.createElement('div');
         pDivImageNode.setAttribute('id', 'person-image');
         pListRowsNode.appendChild(pDivImageNode);
-        
+
         pImageNode = document.createElement('img');
-        pImageNode.setAttribute('src', 'img/person/' +  rowCells[0]);
+        pImageNode.setAttribute('src', 'img/person/' + rowCells[0]);
         pImageNode.setAttribute('width', '130');
         pImageNode.setAttribute('height', '130');
         pDivImageNode.appendChild(pImageNode);
-        
+
         pDivContentNode = document.createElement('div');
         pDivContentNode.setAttribute('id', 'person-content');
         pDivContentNode.setAttribute('class', 'person-content');
         pListRowsNode.appendChild(pDivContentNode);
-        
+
         //Person-Name
         pDivPersonNameNode = document.createElement('div');
         pDivPersonNameNode.setAttribute('id', 'person-name');
@@ -123,7 +126,7 @@ function processPersonData(data) {
         pSpanPersonInstituteTextNode = document.createTextNode(rowCells[3]);
         pSpanPersonInstituteNode.appendChild(pSpanPersonInstituteTextNode);
         pDivPersonInstituteNode.appendChild(pSpanPersonInstituteNode);
-        
+
         //Person-Location -> Navigate?
         pDivPersonLocationNode = document.createElement('div');
         pDivPersonLocationNode.setAttribute('id', 'person-location');
@@ -137,19 +140,19 @@ function processPersonData(data) {
         pDivPersonMailNode = document.createElement('div');
         pDivPersonMailNode.setAttribute('id', 'person-mail');
         pDivContentNode.appendChild(pDivPersonMailNode);
-        
+
         strMail = rowCells[5];
         resMail = strMail.replace("[at]", "@");
-        
+
         pSpanPersonMailNode = document.createElement('span');
         pAPersonMailNode = document.createElement('a');
-        
+
         pAPersonMailNode.setAttribute('href', 'mailto:' + resMail);
         /*pAPersonMailNode.setAttribute('data-rel', 'external');
         pAPersonMailNode.setAttribute('target','_blank');
         pAPersonMailNode.setAttribute('data-role', 'touch');*/
         pAPersonMailTextNode = document.createTextNode(rowCells[5]);
-        
+
         pAPersonMailNode.appendChild(pAPersonMailTextNode);
         pSpanPersonMailNode.appendChild(pAPersonMailNode);
         pDivPersonMailNode.appendChild(pSpanPersonMailNode);
@@ -165,11 +168,23 @@ function processPersonData(data) {
         pAPersonPhoneNode.appendChild(pAPersonPhoneTextNode);
         pSpanPersonPhoneNode.appendChild(pAPersonPhoneNode);
         pDivPersonPhoneNode.appendChild(pSpanPersonPhoneNode);
-        
+
+        if (rowCells[7] != "") {
+            pDivNaviNode = document.createElement("div");
+            pDivNaviNode.setAttribute("id", "navi");
+            pDivContentNode.appendChild(pDivNaviNode);
+
+            pNaviTextNode = document.createTextNode("Navigieren");
+            pNaviNode = document.createElement("a");
+            pNaviNode.setAttribute("href", rowCells[7]);
+            pNaviNode.appendChild(pNaviTextNode);
+            pDivNaviNode.appendChild(pNaviNode);
+        }
+
         pHR = document.createElement('hr');
         pListIconsNode.appendChild(pHR);
     }
-    
+
     //setStyles();
 }
 
