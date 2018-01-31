eaec4c7ccef2a1967611b560cee2ef4f4ba5ac0e
Igor Ivkic <igor.ivkic@fh-burgenland.at>
2018-01-31 22:54:43 +0100

    commit

--- alumni/js/alumni.js
+++
@@ -1,7 +0,0 @@
-/* global window */
-
-function navigate(target) {
-    'use strict';
-    
-    onInApp = window.open(target, '_blank', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
-}
