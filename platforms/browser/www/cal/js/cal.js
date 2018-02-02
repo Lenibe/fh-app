907016a4a33ec56c4f5873a8acc5a8cf67efaad2
Igor Ivkic <Rookee@MacATRON.local>
2018-02-02 13:35:34 +0100

    commit

--- platforms/browser/www/cal/js/cal.js
+++ platforms/browser/www/cal/js/cal.js
@@ -1,7 +1,8 @@
-/*global $, document window*/
+/*global $, document, window*/
 
 function get_height() {
-      return $(window).height();
+    'use strict';
+    return $(window).height();
 }
 
 $(document).ready(function () {
@@ -25,14 +26,14 @@ $(document).ready(function () {
         events: [{ title: 'Präsentation App', start: '2018-02-03', color: lightRed},
                  { title: 'Abgabe Folien Endpräsentation', start: '2018-02-11', color: lightRed},
                  { title: 'Abgabe Selbsevaluierung', start: '2018-02-11', color: lightRed},
-                 { title: 'Geburtstag :)', start: '2018-02-12', },
+                 { title: 'Geburtstag :)', start: '2018-02-12'},
                  {title: 'Ferien', start: '2018-02-04', end: '2018-02-18', color: lightGreen},
                  {title: 'Conference', start: '2018-02-14', end: '2018-02-16', color: lightBlue, textColor: white},
                  {title: 'Valentinstagsessen', start: '2018-02-14T18:30:00', end: '2018-02-14T21:30:00', allDay: false},
                  {title: 'Empirisches Forschungsdesign fertig schreiben', start: '2018-02-24', end: '2018-02-25', color: lightBlue, textColor: white},
                  {title: 'Endabgabe empirisches Forschungsdesign!!!', start: '2018-02-26T23:59:59', color: lightRed, allDay: false},
-                 {title: 'MRW-ALV E.GH.HS2 - MIMK-3G2', start: '2018-03-03T08:30:00', end: '2018-03-03T12:15:00', color: lightRed, allDay: false},
-                 {title: 'MRW-ALV E.GH.HS2 - MIMK-3G2', start: '2018-03-03T12:15:00', end: '2018-03-03T13:45:00', color: lightRed, allDay: false}
+                 {title: 'MRW-ALV E.HG.HS1 - MIMK-3G2', start: '2018-03-03T08:30:00', end: '2018-03-03T12:15:00', color: lightRed, allDay: false, url: '../navi/enavi.html?start=0,5,15&end=2,6,6&ani=true'},
+                 {title: 'MRW-ALV E.HG.HS2 - MIMK-3G2', start: '2018-03-03T12:15:00', end: '2018-03-03T13:45:00', color: lightRed, allDay: false, url: '../navi/enavi.html?start=0,5,15&end=2,6,10&ani=true'}
                 ],
                 timeFormat: 'H(:mm)'
     });
